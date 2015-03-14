f43b6474e43e01bf9911fdc59fd990d6416439f2
polkmnjhb <polkmnjhb@hotmail.com.tw>
2015-03-14 21:17:55 +0800

    2

--- coloregg1.c
+++ coloregg1.c
@@ -3,43 +3,36 @@
 
 int main()
 {
+	int x;
 	int i;
 	for(i=0;i<9 ;++i)
 	{
-		if(i%7==1)
-		{ 
+		x=i%7;
+		switch(x)
+		{
+		case 0:
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY | FOREGROUND_RED);
-		printf("egg-%d\n",i+1);
-		} 
-		else if(i%7==2)
-		{ 
+		break;
+		case 1:
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY | FOREGROUND_GREEN);
-		printf("egg-%d\n",i+1);
-		} 
-		else if(i%7==3)
-		{ 
+		break;
+		case 2:
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY | FOREGROUND_BLUE);
-		printf("egg-%d\n",i+1);
-		} 
-		else if(i%7==4)
-		{ 
+		break;
+		case 3:
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN);
-		printf("egg-%d\n",i+1);
-		} 
-		else if(i%7==5)
-		{ 
+		break;
+		case 4:
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_BLUE);
-		printf("egg-%d\n",i+1);
-		} 
-		else if(i%7==6)
-		{ 
+		break;
+		case 5:
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_BLUE);
-		printf("egg-%d\n",i+1);
-		} 
-		else if(i%7==0)
-		{ 
+		break;
+		case 6:
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-		printf("egg-%d\n",i+1);
-		} 
+		break;
+		}
+	printf("egg-%d\n",i+1);
 	}
+	
 }
